8f43b1edc1ae88766b157fe596f4292d6121a498
Tri Dang <tri.dang@neovation.com>
2022-10-24 11:52:17 -0500

    added an E2E test

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('visit my app', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/')
+    })
+  
+    it('check my 1st paragraph', () => {
+      cy.log('I am here')
+      cy.get('.jsx-ed91f0f1513cbcbf.main > :nth-child(1) > .jsx-a5cf438eb37df449').should('have.text','My first blog post')
+    })
+  })
+  
