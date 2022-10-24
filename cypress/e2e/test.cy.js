26b4057456ef503e08f53a6ee697264541339872
Tri Dang <tri.dang@neovation.com>
2022-10-24 12:48:00 -0500

    added new changes into testcyjs

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -8,6 +8,8 @@ describe('visit my app', () => {
     it('check my 1st paragraph', () => {
       cy.log('I am here')
       cy.get('.jsx-ed91f0f1513cbcbf.main > :nth-child(1) > .jsx-a5cf438eb37df449').should('have.text','My first blog post')
+      cy.log('git request')
+      cy.log('git request in e2etestfix')
     })
   })
   
